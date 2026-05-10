2855550bcd392fc7923c4afee6e25e107649ed97
iiva47760-ship-it <iiva47760@gmail.com>
2026-05-10 17:48:34 +0200

    fix: remove TonConnect from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import App from './App.tsx'
 import './index.css'
 
-const manifestUrl = 'https://raw.githubusercontent.com/iiva47760-ship-it/xankongcocaking-os/main/tonconnect-manifest.json'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <App />
-    </TonConnectUIProvider>
+    <App />
   </StrictMode>,
 )
